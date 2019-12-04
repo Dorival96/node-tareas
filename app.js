ca8218ce5e67ffc8876d6dfe4e7b3e3e853b348a
Dorival <dpichamba@est.ups.edu.ec>
2019-12-04 00:19:15 -0500

    tercer commit

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
+//Se importa mos módulos necesarios para utilizar sus funciones
 const argv = require('./config/yargs').argv;
+//Se importa el módulo donde se encuentra las funciones que trabajará en las tareas
 const tareas = require('./controlador/tareas-por-hacer');
 const colors = require('colors');
 
 let comando = argv._[0];
 
-//console.log(comando);
+//Un switch será necesario para las 4 opciones de crea, actualizar, borrar y listar 
 switch (comando) {
     case 'crear':
         console.log("=--Crear Tareas--=");
-
+        //Se invoca a la funcion crear que necesita la descripción para la crear tarea
         let tarea = tareas.crear(argv.descripcion);
         console.log(tarea);
         break;
 
     case 'listar':
         console.log("=--Listar Tareas--=");
-
+        //Se llama a la función listar para recibir el estado en completado de la tarea
         let listado = tareas.getLista(argv.completado);
         for (let tarea of listado) {
             console.log("=--POR HACER--=".green);
@@ -26,18 +28,19 @@ switch (comando) {
 
     case 'actualizar':
         console.log("=--Actualizar Tareas--=");
-
+        //Se invoca a la función de actualizar que recibe como parametro la descripcion de la tarea y su estado
         let actualizado = tareas.actualizar(argv.descripcion, argv.completado);
         console.log(actualizado);
         break;
 
     case 'borrar':
         console.log("=--Borrar Tareas--=");
-
+        //Se invoca a la función de borrar que recibe como parámetro a descripcion para saber que tarea borrar
         let borrado = tareas.borrar(argv.descripcion);
         console.log(borrado);
         break;
 
     default:
+        //Es el mensaje que aparecerá cuando el usuario ingrese mal los comandos
         console.log("Comando no reconocido");
 }
